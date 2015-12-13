57b41183d40a2fc5859a68d1ea1d3d5d8835411c
mshibl <mohamed.shibl@hotmail.com>
2015-12-13 04:49:45 -0800

    adds working api loading all cards + adds styling

--- scripts/controllers/mainCtrl.js
+++ scripts/controllers/mainCtrl.js
@@ -1,11 +1,9 @@
 'use strict';
-	google.load("feeds", "1");
 
 angular.module('tedTalksFeed')
 .controller('mainCtrl', function($scope,feedDataService){
 	feedDataService.getFeeds(function(response){
-		$scope.videoFeeds = response.feed.entries;
+		$scope.videoFeeds = response
 		$scope.$apply();
 	})
-
 })
--- scripts/services/dataSer.js
+++ scripts/services/dataSer.js
@@ -1,12 +1,27 @@
 'use strict';
+google.load("feeds", "1");
 
 angular.module('tedTalksFeed')
 .service('feedDataService',function(){
 	this.getFeeds = function(callback){
 		google.setOnLoadCallback(function(){
 			var feed = new google.feeds.Feed("http://feeds.feedburner.com/tedtalks_video");
-	        feed.setNumEntries(100);
-	        feed.load(callback)
+	    feed.setNumEntries(100);
+      feed.load(function(response){
+				var videoFeeds = [];
+				for (var i = 0; i < response.feed.entries.length; i++) {
+					videoFeeds.push({
+						"title": response.feed.entries[i].title,
+						"url": response.feed.entries[i].link,
+						"publishedDate": response.feed.entries[i].publishedDate,
+						"contentSnippet": response.feed.entries[i].contentSnippet,
+						"content": response.feed.entries[i].content.replace(/<\w+((\s+\w+(\s*=\s*(?:.*?|.*?|[^>\s]+))?)+\s*|\s*)>/g,""),
+						"image": response.feed.entries[i]['mediaGroups'][0]['contents'][0]['thumbnails']['0']['url'],
+						"videoUrl": response.feed.entries[i]['mediaGroups'][0]['contents'][0]['url']
+					})
+				}
+				callback(videoFeeds)
+			})
 		});
 	}
 })
