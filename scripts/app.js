c2e7d24feca35ed3cce4e19d443efaebb0875600
mshibl <mohamed.shibl@hotmail.com>
2015-12-14 03:21:47 -0800

    adds videoplayer controller and template, adds css, adds search bar

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,3 @@
-angular.module("tedTalksFeed",[])
+var uibs = require('angular-ui-bootstrap');
+
+angular.module("tedTalksFeed",[uibs,require('angular-sanitize')])
--- scripts/controllers/mainCtrl.js
+++ scripts/controllers/mainCtrl.js
@@ -1,9 +1,31 @@
 'use strict';
 
 angular.module('tedTalksFeed')
-.controller('mainCtrl', function($scope,feedDataService){
+.controller('mainCtrl', function($scope,feedDataService,$uibModal,$log){
+	// Loading Feeds
 	feedDataService.getFeeds(function(response){
 		$scope.videoFeeds = response
 		$scope.$apply();
 	})
+
+	// Opening Video on Page
+  $scope.open = function (videoUrl) {
+  	$scope.videoUrl = videoUrl
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: 'templates/videoPlayer.html',
+      controller: 'ModalInstanceCtrl',
+      backdropClass: 'backdrop',
+      resolve: {
+        videoUrl: function () {
+          return $scope.videoUrl;
+        }
+      }
+    });
+  };
+})
+
+// Video Player Instance
+.controller('ModalInstanceCtrl', function($scope, $uibModalInstance, $sce, videoUrl) {
+	$scope.videoSrc = $sce.trustAsResourceUrl(videoUrl)
 })
