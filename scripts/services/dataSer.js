62224b543c2389247b51d0e1f2eb9f7890a505eb
mshibl <mohamed.shibl@hotmail.com>
2015-12-14 14:15:56 -0800

    fixes loading on phone

--- scripts/services/dataSer.js
+++ scripts/services/dataSer.js
@@ -6,7 +6,7 @@ angular.module('tedTalksFeed')
 	this.getFeeds = function(callback){
 		google.setOnLoadCallback(function(){
 			var feed = new google.feeds.Feed("http://feeds.feedburner.com/tedtalks_video");
-	    feed.setNumEntries(100);
+	    feed.setNumEntries(5);
       feed.load(function(response){
 				var videoFeeds = [];
 				for (var i = 0; i < response.feed.entries.length; i++) {
